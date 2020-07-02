d660a3d87db8b0edc0d3a8384b79e90a97316c9d
kefirm <kefirm@tlen.pl>
2020-07-02 13:55:28 +0200

    quiz vue
    simple quiz

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>My quiz app</b></b-nav-item>
+            <b-nav-item disabled>Counter: {{ numCorrect }} / {{ numTotal }} </b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default  {
+        props: [
+            'numCorrect',
+            'numTotal'
+        ]
+    }
+
+</script>
+
+<style scoped>
+
+</style>
